9e9a47bf882cb413f60c1edf6f1b4f60e381cb15
Abdulkadir Ali Mohammed <dadorap88@hotmail.com>
2024-12-16 18:42:32 +0100

    Modified : ERDiagram.txt Added : HavenHotel.sql Renamed : Migrations/20241216170734_init.Designer.cs Renamed : Migrations/20241216170734_init.cs

---
+++ HavenHotel.sql
@@ -0,0 +1,30 @@
+﻿select * 
+	from Guests;
+
+select *
+	from Rooms;
+
+select * 
+	from Bookings;
+
+Select *
+	from Rooms 
+	where Size < 25;
+
+Select *
+	from Guests g
+	order by g.Name
+
+Select 
+	Guests.Name AS [Name],
+	Bookings.Id AS BookingID,
+	Rooms.RoomNumber AS RoomNumber,
+	Bookings.TotalPrice AS TotalPrice
+	FROM 
+		Bookings
+	Join Rooms
+		on
+		Bookings.RoomId = Rooms.Id
+	join Guests
+		on
+		Bookings.GuestId = Guests.Id;
